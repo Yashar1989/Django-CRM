fc97f65c55a81c121b8439c133b8645fc31daa3c
yashar1989 <yashar1989@gmail.com>
2023-03-27 21:01:20 +0330

    add registrations view

--- website/forms.py
+++ website/forms.py
@@ -4,9 +4,9 @@ from django import forms
 
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class': 'form-control'}))
-    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'First Name'}))
-    last_name = forms.CharField(max_length=250, widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'Last Name'}))
+    email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "Email Address"}))
+    first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'First Name'}))
+    last_name = forms.CharField(label="", max_length=250, widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'Last Name'}))
     
     class Meta:
         model = User
