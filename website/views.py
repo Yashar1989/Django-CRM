edaa6b413e238d2450886bd1772f67524926a3f9
yashar1989 <yashar1989@gmail.com>
2023-03-27 17:40:48 +0330

    add login section

--- website/views.py
+++ website/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 
 
 def home(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        print(username, password)
+        # Authenticate user
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in successfully")
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid username or password", extra_tags='danger')
+            return redirect('home')
+
+
     return render(request, 'website/home.html', {})
 
 def login_user(request):
