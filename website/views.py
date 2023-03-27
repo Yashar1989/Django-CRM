ab0dc2b92753f1cf4a0dddf15ac9928c9d1d7d66
yashar1989 <yashar1989@gmail.com>
2023-03-28 00:45:42 +0330

    Final Project

--- website/views.py
+++ website/views.py
@@ -77,3 +77,16 @@ def add_record(request):
         return render(request, 'website/add_record.html', {'form': form})
     messages.success(request, "You Must Be Logged In...", extra_tags='danger')
     return redirect('home')
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(pk=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record Has Been Updated..", extra_tags='success')
+            return redirect('home')
+        return render(request, 'website/update_record.html', {'form': form})
+    messages.success(request, "You Must Be Logged In...", extra_tags='danger')
+    return redirect('home')
+
